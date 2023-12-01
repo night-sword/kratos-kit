fe73322f9fecef02707c71228715a6e78fef3d19
night-sword <night-sword>
2023-12-01 04:10:47 -0600

    add go-safe

--- async/go-safe.go
+++ async/go-safe.go
@@ -2,7 +2,7 @@ package util
 
 import "github.com/night-sword/kratos-kit/log"
 
-func GoSafe[T any](fn func(T)) func(T) {
+func Go[T any](fn func(T)) func(T) {
 	return func(p T) {
 		go func() {
 			defer func() {
@@ -16,7 +16,7 @@ func GoSafe[T any](fn func(T)) func(T) {
 	}
 }
 
-func GoSafeRun(fn func()) {
+func Run(fn func()) {
 	go func() {
 		defer func() {
 			if err := recover(); err != nil {
